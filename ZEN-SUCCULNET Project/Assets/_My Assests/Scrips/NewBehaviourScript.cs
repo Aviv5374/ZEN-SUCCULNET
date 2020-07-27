42fb248cad385ae04cb5a11cd330f3a01b2a4bb5
Aviv5374 <aviv5374@gmail.com>
2020-07-27 09:48:34 +0300

    First Unity Setup 2

---
+++ ZEN-SUCCULNET Project/Assets/_My Assests/Scrips/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{   
+
+    #region Initialization
+
+	void Awake()
+	{
+		
+	}
+			
+    void Start()
+    {
+        
+    }
+     
+    #endregion
+
+    #region Updating	 
+
+    void Update()
+    {
+        
+    }
+	
+	#endregion
+		
+}
